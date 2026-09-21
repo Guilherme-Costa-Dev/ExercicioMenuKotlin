39a4231da0d5f4d5d4ffc9a89bf689cd3ecc6d64
¨Guilherme¨ <guilhermecosta030409@gmail.com>
2026-09-21 19:58:49 -0300

    finalizado

--- app/src/main/java/com/guilherme/exerciciomenukotlin/MainActivity.kt
+++ app/src/main/java/com/guilherme/exerciciomenukotlin/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.activity.addCallback
 import com.google.android.material.navigation.NavigationView
 import com.guilherme.exerciciomenukotlin.databinding.ActivityMainBinding
 
@@ -21,10 +25,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Hide the status bar.
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.statusBars())
-
+        WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.systemBars())
         setSupportActionBar(binding.toolbar)
 
         val toggle = ActionBarDrawerToggle(
@@ -34,12 +36,51 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        // supportActionBar?.title = ""
         binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        fragmentManager = supportFragmentManager
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_cart -> openFragment(CartFragment())
+                R.id.botton_discount -> openFragment(DescontosFragment())
+                R.id.bottom_menu -> openFragment(MenuFragment())
+            }
+            true
+        }
+
+        openFragment(HomeFragment())
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Adicionar", Toast.LENGTH_SHORT).show()
+        }
+
+        onBackPressedDispatcher.addCallback(this){
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            }else{
+                finish()
+            }
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Implementação do clique dos itens do menu lateral virá na sequência
+        when(item.itemId){
+            R.id.nav_carrinho -> openFragment(CartFragment())
+            R.id.nav_datas -> openFragment(CalendarFragment())
+            R.id.nav_nacionais -> openFragment(NacionalFragment())
+            R.id.nav_oferta -> Toast.makeText(this, "Ofertas", Toast.LENGTH_SHORT).show()
+            R.id.nav_populares -> Toast.makeText(this, "Populares", Toast.LENGTH_SHORT).show()
+            R.id.nav_internacionais -> Toast.makeText(this, "Internacionais", Toast.LENGTH_SHORT).show()
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun openFragment(fragment: Fragment){
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
